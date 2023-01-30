2e278988255c2200f140e690bc660e32b525927a
heinokesoe <heinokesoe369@gmail.com>
2023-01-30 16:33:47 +0630

    update rclone-web-gui

--- rclone-web-gui/entrypoint.sh
+++ rclone-web-gui/entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 set -e
 
-DEFAULT_ARGS="--rc-web-gui --rc-web-gui-no-open-browser --rc-addr :8000"
+DEFAULT_ARGS="--rc-serve --rc-web-gui-no-open-browser --rc-addr :8000 --rc-files /data/rclone"
 CUSTOM_ARGS=""
 
 if [ -n "$USERNAME" ]; then
   CUSTOM_ARGS="--rc-user ${USERNAME}"
+else
+  CUSTOM_ARGS="--rc-user admin"
 fi
 if [ -n "$PASSWORD" ]; then
   CUSTOM_ARGS="${CUSTOM_ARGS} --rc-pass ${PASSWORD}"
+else
+  CUSTOM_ARGS="${CUSTOM_ARGS} --rc-pass admin"
 fi
 
 COMMAND="rclone rcd ${DEFAULT_ARGS} ${CUSTOM_ARGS}"
